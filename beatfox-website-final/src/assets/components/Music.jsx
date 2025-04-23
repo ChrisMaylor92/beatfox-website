ff2f0774d8982036a68056b0e8ac46cc1e941b17
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-23 22:39:05 +0100

    minor changes

--- beatfox-website-final/src/assets/components/Music.jsx
+++ beatfox-website-final/src/assets/components/Music.jsx
@@ -2,8 +2,8 @@ import popEye from "../media/pop-eye.png"
 export const Music = () => {
     return (
         <div id="music">
-            <h1 id="music-title">Error 420: page in development</h1>
-            <img src={popEye} className="music-img"></img>
+            <h1 id="music-title">Page currently in development</h1>
+            
 
         </div>
     )
