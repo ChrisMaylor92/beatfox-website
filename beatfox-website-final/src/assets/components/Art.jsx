ae35d2621f71efd253af776787f4a29b2e29313e
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-24 22:55:13 +0100

    adds animation to home page on first load

--- beatfox-website-final/src/assets/components/Art.jsx
+++ beatfox-website-final/src/assets/components/Art.jsx
@@ -3,10 +3,13 @@ import { Portfolio } from './Portfolio'
 import { Header } from './Header'
 import { Gallery } from './Gallery'
 import { Bio } from './Bio'
-
+import { useState, useEffect, useRef } from "react";
 export const Art = () => {
+    
+    
     return (
-        <div>
+        <div >
+            
             <Header />
             <Bio />
             <Portfolio />
--- beatfox-website-final/src/assets/components/Home.jsx
+++ beatfox-website-final/src/assets/components/Home.jsx
@@ -11,37 +11,87 @@ import {
 
 export const Home = () => {
     const panelRef = useRef(null);
+    const scrollDiv = useRef(null);
+    const [percent, setPercent] = useState(0)
+
+
+    function scrollAnimOne() {
+    
+        setTimeout(() => {
+            window.scrollTo({
+                top: 620,
+                left: 0,
+                behavior: "smooth",
+              });
+        }, 1000); 
+        setTimeout(() => {
+            window.scrollTo({
+                top: 174,
+                left: 0,
+                behavior: "smooth",
+              });
+        }, 2000); 
+        setTimeout(() => {
+            window.scrollTo({
+                top: 500,
+                left: 0,
+                behavior: "smooth",
+              });
+        }, 3000);
+        setTimeout(() => {
+            window.scrollTo({
+                top: 334,
+                left: 0,
+                behavior: "smooth",
+              });
+        }, 4000); 
+        
+
+    } 
+
+    useEffect(() => { 
+        if (scrollDiv.current) {
+            console.log(scrollDiv.current.scrollTop, "<<<")
+            console.log(scrollDiv.current.scrollHeight, ">>>");
+          }
+        scrollAnimOne()
+        
+    }, [])
 
     function getScrollPercent() {
         var h = document.documentElement, 
             b = document.body,
             st = 'scrollTop',
             sh = 'scrollHeight';
+       
         return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
     }
     
     window.onscroll = () => {
         const panel = panelRef.current;
+        const scrollPercent = getScrollPercent()
+        
         if (panel) {
-            panel.resize(getScrollPercent());
+            panel.resize(scrollPercent);
         }
-        console.log(getScrollPercent())
+        
     }
     
     return (
-        <div id="home">
+        <div id="home" ref={scrollDiv}>
         
             <PanelGroup direction="vertical" id="panel-group"> 
-                <Panel  ref={panelRef} minSize={7} order={1}  className="panel" id="music-panel">
+                <Panel  ref={panelRef} defaultSize={7} minSize={7} order={1}  className="panel" id="music-panel">
                     <div id="home-music-container">
                         <video id="home-music-video" autoPlay loop muted src={homeVideoMusic}></video>
-                        <Link to="/music" id="home-music-title">
+                        
+                        <Link to="/music" id="home-music-title" >
                             <h1 >Music</h1>
                         </Link>
                     </div>
                 </Panel>
-                <PanelResizeHandle/>
-                <Panel minSize={0} order={1} className="panel">
+                {/* <PanelResizeHandle/> */}
+                <Panel minSize={0} order={1} className="panel" >
                     <div id="home-art-container">
                         <video id="home-art-video" autoPlay loop muted src={homeVideoArt}></video>  
                         <Link to="/art" >
