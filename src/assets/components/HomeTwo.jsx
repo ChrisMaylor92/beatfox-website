470eabaf99d661eec2a45cea06f76f57843b1614
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-30 17:24:55 +0100

    mobile-testing

--- src/assets/components/HomeTwo.jsx
+++ src/assets/components/HomeTwo.jsx
@@ -1,5 +1,29 @@
 import beatfoxRows from "../media/beatfox-rows.jpg"
+import { useState, useEffect, useRef } from "react";
 export const HomeTwo = () => {
+
+
+    useEffect(() => {
+        const container = document.getElementById("img-container")
+
+        function getScrollPercent() {
+            const st = 'scrollTop'
+            const sh = 'scrollHeight';
+           
+            return (container[st]) / ((container[sh]) - container.clientHeight) * 100;
+        }
+
+        container.onscroll = () => {
+            console.log(container.scrollTop, "scrollTop")
+            console.log(container.scrollHeight, "scrollHeight")
+            console.log(container.clientHeight, "clientHeight")
+            console.log(getScrollPercent(), "percent")
+            
+        }
+    }, [])
+    
+   
+
     return (
     <div id="home-two">
         <div id="img-container">
