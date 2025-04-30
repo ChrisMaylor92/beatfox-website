f92711343830df4ac6a0f3dda818bd65e1c1b898
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-30 17:00:34 +0100

    mobile teeesting

--- src/assets/components/HomeTwo.jsx
+++ src/assets/components/HomeTwo.jsx
@@ -2,7 +2,11 @@ import beatfoxRows from "../media/beatfox-rows.jpg"
 export const HomeTwo = () => {
     return (
     <div id="home-two">
-        <img src={beatfoxRows} id="test-img"></img>
+        <div id="img-container">
+            <img src={beatfoxRows} id="test-img"></img>
+            <img src={beatfoxRows} id="test-img-2"></img>
+        </div>
+        
     </div>
     )
 }
