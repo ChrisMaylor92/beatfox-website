fccc2f1a961c1d6288aa91a08fe09c6fe04a6284
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-30 18:10:32 +0100

    mobile testing

--- src/assets/components/HomeTwo.jsx
+++ src/assets/components/HomeTwo.jsx
@@ -1,10 +1,14 @@
 import beatfoxRows from "../media/beatfox-rows.jpg"
+import homeVideoArt from "../media/home-art.mp4"
+import homeVideoMusic from "../media/home-music.mp4"
+import japan from "../media/collab-japanese.png"
+import { Link } from 'react-router-dom';
 import { useState, useEffect, useRef } from "react";
 export const HomeTwo = () => {
 
 
     useEffect(() => {
-        const container = document.getElementById("img-container")
+        const container = document.getElementById("scroll-container")
 
         function getScrollPercent() {
             const st = 'scrollTop'
@@ -26,9 +30,22 @@ export const HomeTwo = () => {
 
     return (
     <div id="home-two">
-        <div id="img-container">
-            <img src={beatfoxRows} id="test-img"></img>
-            <img src={beatfoxRows} id="test-img-2"></img>
+        <div id="scroll-container">
+
+
+            
+            <div id="homeTwo-music-container">
+                <video id="homeTwo-music-video" autoPlay loop muted src={homeVideoMusic}></video>
+                <Link to="/music" id="homeTwo-music-title" >
+                    <h1 >Music</h1>
+                </Link>
+            </div>
+            <div id="homeTwo-art-container">
+                <video id="homeTwo-art-video" autoPlay loop muted src={homeVideoArt}></video>  
+                <Link to="/art" >
+                    <h1 id="homeTwo-art-title">Art</h1>
+                </Link>
+            </div>   
         </div>
         
     </div>
