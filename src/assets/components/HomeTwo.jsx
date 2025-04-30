2b82554ba44b48e37a648c5da49e73b3b9b81fa5
Chris Maylor <chrismaylor1@hotmail.com>
2025-04-30 16:25:23 +0100

    mobile-testing

---
+++ src/assets/components/HomeTwo.jsx
@@ -0,0 +1,8 @@
+import beatfoxRows from "../media/beatfox-rows.jpg"
+export const HomeTwo = () => {
+    return (
+    <div id="home-two">
+        <img src={beatfoxRows} id="test-img"></img>
+    </div>
+    )
+}
