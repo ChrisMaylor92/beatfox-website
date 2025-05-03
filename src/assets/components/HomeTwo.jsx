bf8659b4f651aa98ab246364e9c89e143ffba0c1
Chris Maylor <chrismaylor1@hotmail.com>
2025-05-03 14:45:47 +0100

    mobile testing

--- src/assets/components/HomeTwo.jsx
+++ src/assets/components/HomeTwo.jsx
@@ -14,34 +14,19 @@ import {
 
 
 export const HomeTwo = () => {
+    const panelRef = useRef(null);
 
-
-    // useEffect(() => {
+    useEffect(() => {
+        const panel = panelRef.current;
         
-    //     const container = document.getElementById("scroll-container")
-
-    //     function getScrollPercent() {
-    //         const st = 'scrollTop'
-    //         const sh = 'scrollHeight';
-           
-    //         return (container[st]) / ((container[sh]) - container.clientHeight) * 100;
-    //     }
-
-    //     container.onscroll = () => {
-    //         console.log(container.scrollTop, "scrollTop")
-    //         console.log(container.scrollHeight, "scrollHeight")
-    //         console.log(container.clientHeight, "clientHeight")
-    //         console.log(getScrollPercent(), "percent")
-            
-    //     }
-    // }, [])
+    }, [])
     
    
 
     return (
     <div id="home-two">
         <PanelGroup direction="vertical" id="panel-group"> 
-                <Panel defaultSize={45}  minSize={0} order={1} className="panel" >
+                <Panel ref={panelRef} defaultSize={42}  minSize={0} order={1} className="panel" >
                     <div id="homeTwo-music-container">
                         <video id="homeTwo-music-video" autoPlay loop muted src={homeVideoMusic}></video>     
                         <Link to="/music" id="homeTwo-music-title" >
@@ -63,16 +48,7 @@ export const HomeTwo = () => {
                     </div>
                 </Panel>
             </PanelGroup>
-        {/* <div id="scroll-container">
-
-            
-
-
-            
-                    
-        <img id="test-img-1" src={beatfoxRows}/>
-        <img id="test-img-2" src={beatfoxRows}/>
-        </div> */}
+       
         
     </div>
     )
