52d2bdff509131949bc00ec01f51125467381635
Chris Maylor <chrismaylor1@hotmail.com>
2025-05-03 14:08:12 +0100

    mobile testing

--- src/assets/components/HomeTwo.jsx
+++ src/assets/components/HomeTwo.jsx
@@ -41,7 +41,7 @@ export const HomeTwo = () => {
     return (
     <div id="home-two">
         <PanelGroup direction="vertical" id="panel-group"> 
-                <Panel   minSize={0} order={1} className="panel" >
+                <Panel defaultSize={45}  minSize={0} order={1} className="panel" >
                     <div id="homeTwo-music-container">
                         <video id="homeTwo-music-video" autoPlay loop muted src={homeVideoMusic}></video>     
                         <Link to="/music" id="homeTwo-music-title" >
@@ -51,12 +51,15 @@ export const HomeTwo = () => {
                 </Panel>
       
                 <PanelResizeHandle id="homeTwo-pandle" />
-                <Panel minSize={7} order={1} className="panel" >
+                <Panel minSize={9} order={1} className="panel" >
                     <div id="homeTwo-art-container">
-                        <video id="homeTwo-art-video" autoPlay loop muted src={homeVideoArt}></video>  
-                        <Link to="/art" id="homeTwo-art-title">
-                            <h1>Art</h1>
-                        </Link>
+                        <div id="video-and-title">
+                            <video id="homeTwo-art-video" autoPlay loop muted src={homeVideoArt}></video>  
+                            <Link to="/art" id="homeTwo-art-title">
+                                <h1>Art</h1>
+                            </Link>
+                        </div>
+                        
                     </div>
                 </Panel>
             </PanelGroup>
